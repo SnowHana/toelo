5e603b5d5e31badcd54fb271d67346e8b8ec94f4
Nautilus-Ubuntu <wujindaniel1011@gmail.com>
2025-05-29 20:09:20 +0900

    langchain chatbot works, integrate with database

--- src/toelo/frontend/streamlit_app/pages/7_chat_bot.py
+++ src/toelo/frontend/streamlit_app/pages/7_chat_bot.py
@@ -1,31 +1,27 @@
 import streamlit as st
-from openai import OpenAI, api_key
 from toelo.frontend.streamlit_app.display_data import get_player_data
-import os
-from dotenv import load_dotenv, dotenv_values
+from langchain_community.llms import GPT4All
+from pathlib import Path
 
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+# home = Path.home()
+model_path = Path.home() / Path("models") / "mistral-7b-openorca.Q4_0.gguf"
+print(model_path)
+model = GPT4All(model=str(model_path), n_threads=8)
+response = model.invoke("Once upon a time, ")
 
-
-def chat(query):
-    # query = """SELECT * FROM players_elo WHERE elo IS NOT NULL ORDER BY elo DESC LIMIT 200;"""
-
-    client = OpenAI(api_key=api_key)
-
-    response = client.responses.create(model="gpt-4o", input=query)
-    # st.markdown(response.output_text)
-    return response.output_text
+print(response)
+# def chat(query):
+# query = """SELECT * FROM players_elo WHERE elo IS NOT NULL ORDER BY elo DESC LIMIT 200;"""
 
 
-st.set_page_config(page_title="ChatBot", page_icon="📊")
+# st.set_page_config(page_title="ChatBot", page_icon="📊")
 
-st.markdown("# Have a chat")
-st.sidebar.header("Have a chat")
+# st.markdown("# Have a chat")
+# st.sidebar.header("Have a chat")
 
-prompt = st.text_input("Enter your prompt:")
+# prompt = st.text_input("Enter your prompt:")
 
-if st.button("Get Response"):
-    response = chat(prompt)
-    if response:
-        st.write(f"Response: {response}")
+# if st.button("Get Response"):
+#     response = chat(prompt)
+#     if response:
+#         st.write(f"Response: {response}")
