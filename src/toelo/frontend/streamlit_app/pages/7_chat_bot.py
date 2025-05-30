d6c1ebd12270d47be054874af2c9d36cfe7a968c
Nautilus-Ubuntu <wujindaniel1011@gmail.com>
2025-05-30 15:37:25 +0900

    Following tutorial to implement agents

--- src/toelo/frontend/streamlit_app/pages/7_chat_bot.py
+++ src/toelo/frontend/streamlit_app/pages/7_chat_bot.py
@@ -1,9 +1,13 @@
-import streamlit as st
-from toelo.frontend.streamlit_app.display_data import get_player_data
-from langchain_community.llms import GPT4All
 from pathlib import Path
+import gpt4all
+import streamlit as st
+from typing_extensions import TypedDict
 from langchain_community.utilities import SQLDatabase
+from langchain_community.llms import GPT4All
+
+from toelo.frontend.streamlit_app.display_data import get_player_data
 from toelo.player_elo.database_connection import get_connection_string, DATABASE_CONFIG
+
 
 db_uri = get_connection_string(DATABASE_CONFIG)
 print(db_uri)
@@ -13,9 +17,23 @@ print(db.dialect)
 print(db.get_usable_table_names())
 
 
+class State(TypedDict):
+    question: str
+    query: str
+    result: str
+    answer: str
+
+
+from langchain.chat_models import init_chat_model
+
+# Chat model
+llm_path = Path.home() / Path("models") / "mistral-7b-openorca.Q4_0.gguf"
+llm = GPT4All(model=str(llm_path), n_thread=8)
+
+# Toolkit
+
 # print(db.run("SELECT * FROM players_elo LIMIT 10;"))
 # home = Path.home()
-# model_path = Path.home() / Path("models") / "mistral-7b-openorca.Q4_0.gguf"
 # print(model_path)
 # model = GPT4All(model=str(model_path), n_threads=8)
 # response = model.invoke("Once upon a time, ")
